1c945ee543c0d5ac986976c8f32c9f6003c89aff
Xinkuu <kieransaw0721@gmail.com>
2024-03-27 00:05:18 +0800

    Added Pre-processing and reshaping

--- Facial Emotion Detection/data_extract.py
+++ Facial Emotion Detection/data_extract.py
@@ -2,5 +2,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 import pandas as pd
+import tensorflow
+import keras
+from keras.utils import to_categorical
+from keras.utils import plot_model
+from keras.models import Model
+from keras.layers import Input, Dense, Flatten, Dropout, BatchNormalization
+from keras.optimizers import Adam, SGD
+from keras.regularizers import l1, l2
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
 df = pd.read_csv('fer2013.csv')
-print(df.head())
+#print(df.head())
+
+X_train = []
+y_train = []
+X_test = []
+y_test = []
+for index, row in df.iterrows():
+    k = row['pixels'].split(" ")
+    if row['Usage'] == 'Training':
+        X_train.append(np.array(k))
+        y_train.append(row['emotion'])
+    elif row['Usage'] == 'PublicTest':
+        X_test.append(np.array(k))
+        y_test.append(row['emotion'])
+
+X_train = np.array(X_train, dtype = 'uint8')
+y_train = np.array(y_train, dtype = 'uint8')
+print(X_train.shape)
+X_test = np.array(X_test, dtype = 'uint8')
+y_test = np.array(y_test, dtype = 'uint8')
+
+y_train= to_categorical(y_train, num_classes=7)
+y_test = to_categorical(y_test, num_classes=7)
+
+X_train = X_train.reshape(X_train.shape[0], 48, 48, 1)
+X_test = X_test.reshape(X_test.shape[0], 48, 48, 1)
+
+print(X_train.shape)
+#X_train[0]
