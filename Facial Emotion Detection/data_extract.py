54ef1893a4c087bc0d943ff1b59d32c6cb7a3072
Xinkuu <kieransaw0721@gmail.com>
2024-04-02 20:34:34 +0800

    updated data_extract.py to train the CNN.

--- Facial Emotion Detection/data_extract.py
+++ Facial Emotion Detection/data_extract.py
@@ -10,8 +10,72 @@ from keras.models import Model
 from keras.layers import Input, Dense, Flatten, Dropout, BatchNormalization
 from keras.optimizers import Adam, SGD
 from keras.regularizers import l1, l2
+from keras.src.legacy.preprocessing.image import ImageDataGenerator
 from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import concatenate
+
+def FER_Model(input_shape=(48,48,1)):
+    # first input model
+    visible = Input(shape=input_shape, name='input')
+    num_classes = 7
+    #the 1-st block
+    conv1_1 = Conv2D(64, kernel_size=3, activation='relu', padding='same', name = 'conv1_1')(visible)
+    conv1_1 = BatchNormalization()(conv1_1)
+    conv1_2 = Conv2D(64, kernel_size=3, activation='relu', padding='same', name = 'conv1_2')(conv1_1)
+    conv1_2 = BatchNormalization()(conv1_2)
+    pool1_1 = MaxPooling2D(pool_size=(2,2), name = 'pool1_1')(conv1_2)
+    drop1_1 = Dropout(0.3, name = 'drop1_1')(pool1_1)#the 2-nd block
+    conv2_1 = Conv2D(128, kernel_size=3, activation='relu', padding='same', name = 'conv2_1')(drop1_1)
+    conv2_1 = BatchNormalization()(conv2_1)
+    conv2_2 = Conv2D(128, kernel_size=3, activation='relu', padding='same', name = 'conv2_2')(conv2_1)
+    conv2_2 = BatchNormalization()(conv2_2)
+    conv2_3 = Conv2D(128, kernel_size=3, activation='relu', padding='same', name = 'conv2_3')(conv2_2)
+    conv2_2 = BatchNormalization()(conv2_3)
+    pool2_1 = MaxPooling2D(pool_size=(2,2), name = 'pool2_1')(conv2_3)
+    drop2_1 = Dropout(0.3, name = 'drop2_1')(pool2_1)#the 3-rd block
+    conv3_1 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv3_1')(drop2_1)
+    conv3_1 = BatchNormalization()(conv3_1)
+    conv3_2 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv3_2')(conv3_1)
+    conv3_2 = BatchNormalization()(conv3_2)
+    conv3_3 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv3_3')(conv3_2)
+    conv3_3 = BatchNormalization()(conv3_3)
+    conv3_4 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv3_4')(conv3_3)
+    conv3_4 = BatchNormalization()(conv3_4)
+    pool3_1 = MaxPooling2D(pool_size=(2,2), name = 'pool3_1')(conv3_4)
+    drop3_1 = Dropout(0.3, name = 'drop3_1')(pool3_1)#the 4-th block
+    conv4_1 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv4_1')(drop3_1)
+    conv4_1 = BatchNormalization()(conv4_1)
+    conv4_2 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv4_2')(conv4_1)
+    conv4_2 = BatchNormalization()(conv4_2)
+    conv4_3 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv4_3')(conv4_2)
+    conv4_3 = BatchNormalization()(conv4_3)
+    conv4_4 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv4_4')(conv4_3)
+    conv4_4 = BatchNormalization()(conv4_4)
+    pool4_1 = MaxPooling2D(pool_size=(2,2), name = 'pool4_1')(conv4_4)
+    drop4_1 = Dropout(0.3, name = 'drop4_1')(pool4_1)
+    
+    #the 5-th block
+    conv5_1 = Conv2D(512, kernel_size=3, activation='relu', padding='same', name = 'conv5_1')(drop4_1)
+    conv5_1 = BatchNormalization()(conv5_1)
+    conv5_2 = Conv2D(512, kernel_size=3, activation='relu', padding='same', name = 'conv5_2')(conv5_1)
+    conv5_2 = BatchNormalization()(conv5_2)
+    conv5_3 = Conv2D(512, kernel_size=3, activation='relu', padding='same', name = 'conv5_3')(conv5_2)
+    conv5_3 = BatchNormalization()(conv5_3)
+    conv5_4 = Conv2D(512, kernel_size=3, activation='relu', padding='same', name = 'conv5_4')(conv5_3)
+    conv5_3 = BatchNormalization()(conv5_3)
+    pool5_1 = MaxPooling2D(pool_size=(2,2), name = 'pool5_1')(conv5_4)
+    drop5_1 = Dropout(0.3, name = 'drop5_1')(pool5_1)#Flatten and output
+    flatten = Flatten(name = 'flatten')(drop5_1)
+    ouput = Dense(num_classes, activation='softmax', name = 'output')(flatten)# create model 
+    model = Model(inputs =visible, outputs = ouput)
+    # summary layers
+    print(model.summary())
+    
+    return model
+
 df = pd.read_csv('fer2013.csv')
 #print(df.head())
 
@@ -42,3 +106,33 @@ X_test = X_test.reshape(X_test.shape[0], 48, 48, 1)
 
 print(X_train.shape)
 #X_train[0]
+
+datagen = ImageDataGenerator( 
+    rescale=1./255,
+    rotation_range = 10,
+    horizontal_flip = True,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    fill_mode = 'nearest')
+testgen = ImageDataGenerator(rescale=1./255)
+datagen.fit(X_train)
+batch_size = 64
+
+train_flow = datagen.flow(X_train, y_train, batch_size=batch_size) 
+test_flow = testgen.flow(X_test, y_test, batch_size=batch_size)
+
+model = FER_Model()
+opt = Adam(learning_rate=0.0001, decay=1e-6)
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+num_epochs = 100  
+steps_per_epoch = int(len(X_train) / batch_size)
+validation_steps = int(len(X_test) / batch_size)
+
+history = model.fit(train_flow, 
+                    steps_per_epoch=steps_per_epoch, 
+                    epochs=num_epochs,  
+                    verbose=1,  
+                    validation_data=test_flow,
+                    validation_steps=validation_steps)
+
