ef9fd8a9e7e47d8b2efc9fb1e2c773e20eec189a
Tiger <97230376+githubuser-acf21jl@users.noreply.github.com>
2024-05-15 14:36:04 +0100

    Update subscriber_cam.py

--- core/subscriber_cam.py
+++ core/subscriber_cam.py
@@ -9,7 +9,7 @@ from tensorflow.keras.models import load_model, model_from_json
 from tensorflow.keras.preprocessing.image import load_img,img_to_array
 from tensorflow.keras.preprocessing import image
 
-
+# load the model from model.json and the weights from model.h5
 model = model_from_json(open("model.json", "r").read())
 model.load_weights('model.h5')
 
@@ -19,7 +19,9 @@ class CamSubscriber(object):
         The following code will display MiRo's camera data
     """
     def __init__(self, normalised = True):
+        # # uncomment this if you want to run subscriber_cam.py alone, and comment it if you are going to run ./client_demo.py
         # rospy.init_node("cam_subscriber")
+        
         # OpenCV bridge is used for converting ROS img data to data that can be used by OpenCV
         self.bridge = CvBridge()
         self.cam_data = [None, None]
@@ -53,31 +55,35 @@ class CamSubscriber(object):
         """
         Callback function executed upon image arrival
         """
-        # emotion_prediction = None
+        
         cam_data = [None, None]
         
         try:
+            # start predicting emotion if there is no predicted emotion
             if self.emotion_prediction == None: 
                 # Convert compressed ROS image to raw CV image
                 image = self.bridge.compressed_imgmsg_to_cv2(ros_image, "bgr8")
                 # Store image as class attribute for further use
                 cam_data[index] = image
                 face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+                # Convert into grey image
                 grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                # Set how many neighbors each window should have for the area to be considered a face
                 faces = face_haar_cascade.detectMultiScale(grey_image, scaleFactor=1.1, minNeighbors=3)
+                # Put a rectangle around the identified faces
                 for (x,y,w,h) in faces:
                     cv2.rectangle(image, (x,y), (x+w,y+h), (0,0,255), thickness=2)
                     roi_grey = grey_image[y-5:y+h+5,x-5:x+w+5]
                     roi_grey = cv2.resize(roi_grey,(48,48))
-                    # print(roi_grey)
                     image_pixels = img_to_array(roi_grey)
                     image_pixels = np.expand_dims(image_pixels, axis = 0)
                     image_pixels /= 255
+                    # Using the model to predict the emotion
                     predictions = model.predict(image_pixels)
                     max_index = np.argmax(predictions[0])
+                    # There are 7 emotion labels
                     emotion_detection = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
                     self.emotion_prediction = emotion_detection[max_index]
-                    # print(self.emotion_prediction)
                 self.cam_data[index] = cam_data[index]
                 rospy.sleep(1)
             else:
@@ -89,10 +95,10 @@ class CamSubscriber(object):
 
 if __name__ == '__main__':
     camera = CamSubscriber()
-    rospy.sleep(0.5) # short wait to make sure everything is initialised
+    # Short wait to make sure everything is initialised
+    rospy.sleep(0.5)
     while not rospy.is_shutdown():
-        # if camera.cam_data[0] is not None:
-        #     cv2.imshow("Left cam", camera.cam_data[0])
+        # We are using the right camera alone to detect emotion
         if camera.cam_data[0] is not None:
             cv2.imshow("Right cam", camera.cam_data[1])
         cv2.waitKey(1)
