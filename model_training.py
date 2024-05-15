11eb3e4a1f0b365c0902b37e0ad718a6c9d5f1bd
Tiger <97230376+githubuser-acf21jl@users.noreply.github.com>
2024-05-15 14:11:39 +0100

    Add files via upload
    python file used to train the model

---
+++ model_training.py
@@ -0,0 +1,143 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+import pandas as pd
+import tensorflow
+import keras
+from keras.utils import to_categorical
+from keras.utils import plot_model
+from keras.models import Model
+from keras.layers import Input, Dense, Flatten, Dropout, BatchNormalization
+from keras.optimizers import Adam, SGD
+from keras.regularizers import l1, l2
+from keras.src.leagcy.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import concatenate
+
+def FER_Model(input_shape=(48,48,1)):
+    # first input model
+    visible = Input(shape=input_shape, name='input')
+    num_classes = 7
+    #the 1-st block
+    conv1_1 = Conv2D(64, kernel_size=3, activation='relu', padding='same', name = 'conv1_1')(visible)
+    conv1_1 = BatchNormalization()(conv1_1)
+    conv1_2 = Conv2D(64, kernel_size=3, activation='relu', padding='same', name = 'conv1_2')(conv1_1)
+    conv1_2 = BatchNormalization()(conv1_2)
+    pool1_1 = MaxPooling2D(pool_size=(2,2), name = 'pool1_1')(conv1_2)
+    drop1_1 = Dropout(0.3, name = 'drop1_1')(pool1_1)#the 2-nd block
+    conv2_1 = Conv2D(128, kernel_size=3, activation='relu', padding='same', name = 'conv2_1')(drop1_1)
+    conv2_1 = BatchNormalization()(conv2_1)
+    conv2_2 = Conv2D(128, kernel_size=3, activation='relu', padding='same', name = 'conv2_2')(conv2_1)
+    conv2_2 = BatchNormalization()(conv2_2)
+    conv2_3 = Conv2D(128, kernel_size=3, activation='relu', padding='same', name = 'conv2_3')(conv2_2)
+    conv2_2 = BatchNormalization()(conv2_3)
+    pool2_1 = MaxPooling2D(pool_size=(2,2), name = 'pool2_1')(conv2_3)
+    drop2_1 = Dropout(0.3, name = 'drop2_1')(pool2_1)#the 3-rd block
+    conv3_1 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv3_1')(drop2_1)
+    conv3_1 = BatchNormalization()(conv3_1)
+    conv3_2 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv3_2')(conv3_1)
+    conv3_2 = BatchNormalization()(conv3_2)
+    conv3_3 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv3_3')(conv3_2)
+    conv3_3 = BatchNormalization()(conv3_3)
+    conv3_4 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv3_4')(conv3_3)
+    conv3_4 = BatchNormalization()(conv3_4)
+    pool3_1 = MaxPooling2D(pool_size=(2,2), name = 'pool3_1')(conv3_4)
+    drop3_1 = Dropout(0.3, name = 'drop3_1')(pool3_1)#the 4-th block
+    conv4_1 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv4_1')(drop3_1)
+    conv4_1 = BatchNormalization()(conv4_1)
+    conv4_2 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv4_2')(conv4_1)
+    conv4_2 = BatchNormalization()(conv4_2)
+    conv4_3 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv4_3')(conv4_2)
+    conv4_3 = BatchNormalization()(conv4_3)
+    conv4_4 = Conv2D(256, kernel_size=3, activation='relu', padding='same', name = 'conv4_4')(conv4_3)
+    conv4_4 = BatchNormalization()(conv4_4)
+    pool4_1 = MaxPooling2D(pool_size=(2,2), name = 'pool4_1')(conv4_4)
+    drop4_1 = Dropout(0.3, name = 'drop4_1')(pool4_1)
+    
+    #the 5-th block
+    conv5_1 = Conv2D(512, kernel_size=3, activation='relu', padding='same', name = 'conv5_1')(drop4_1)
+    conv5_1 = BatchNormalization()(conv5_1)
+    conv5_2 = Conv2D(512, kernel_size=3, activation='relu', padding='same', name = 'conv5_2')(conv5_1)
+    conv5_2 = BatchNormalization()(conv5_2)
+    conv5_3 = Conv2D(512, kernel_size=3, activation='relu', padding='same', name = 'conv5_3')(conv5_2)
+    conv5_3 = BatchNormalization()(conv5_3)
+    conv5_4 = Conv2D(512, kernel_size=3, activation='relu', padding='same', name = 'conv5_4')(conv5_3)
+    conv5_3 = BatchNormalization()(conv5_3)
+    pool5_1 = MaxPooling2D(pool_size=(2,2), name = 'pool5_1')(conv5_4)
+    drop5_1 = Dropout(0.3, name = 'drop5_1')(pool5_1)#Flatten and output
+    flatten = Flatten(name = 'flatten')(drop5_1)
+    ouput = Dense(num_classes, activation='softmax', name = 'output')(flatten)# create model 
+    model = Model(inputs =visible, outputs = ouput)
+    # summary layers
+    print(model.summary())
+    
+    return model
+
+df = pd.read_csv('fer2013.csv')
+#print(df.head())
+
+X_train = []
+y_train = []
+X_test = []
+y_test = []
+for index, row in df.iterrows():
+    k = row['pixels'].split(" ")
+    if row['Usage'] == 'Training':
+        X_train.append(np.array(k))
+        y_train.append(row['emotion'])
+    elif row['Usage'] == 'PublicTest':
+        X_test.append(np.array(k))
+        y_test.append(row['emotion'])
+
+X_train = np.array(X_train, dtype = 'uint8')
+y_train = np.array(y_train, dtype = 'uint8')
+print(X_train.shape)
+X_test = np.array(X_test, dtype = 'uint8')
+y_test = np.array(y_test, dtype = 'uint8')
+
+y_train= to_categorical(y_train, num_classes=7)
+y_test = to_categorical(y_test, num_classes=7)
+
+X_train = X_train.reshape(X_train.shape[0], 48, 48, 1)
+X_test = X_test.reshape(X_test.shape[0], 48, 48, 1)
+
+print(X_train.shape)
+#X_train[0]
+
+datagen = ImageDataGenerator( 
+    rescale=1./255,
+    rotation_range = 10,
+    horizontal_flip = True,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    fill_mode = 'nearest')
+testgen = ImageDataGenerator(rescale=1./255)
+datagen.fit(X_train)
+batch_size = 64
+
+train_flow = datagen.flow(X_train, y_train, batch_size=batch_size) 
+test_flow = testgen.flow(X_test, y_test, batch_size=batch_size)
+
+model = FER_Model()
+opt = Adam(learning_rate=0.0001, decay=1e-6)
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+num_epochs = 100  
+steps_per_epoch = int(len(X_train) / batch_size)
+validation_steps = int(len(X_test) / batch_size)
+
+history = model.fit(train_flow, 
+                    steps_per_epoch=steps_per_epoch, 
+                    epochs=num_epochs,  
+                    verbose=1,  
+                    validation_data=test_flow,
+                    validation_steps=validation_steps)
+
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights("model.h5")
+print("Saved model to disk")
